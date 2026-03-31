8e3adfd78bc50b50e01bce28594aef779d7e9e50
ahmed elbakly <78669298+ahmedelbakly@users.noreply.github.com>
2026-03-31 20:00:21 +0200

    task

---
+++ client/src/data/products.js
@@ -0,0 +1,8 @@
+  const productsData = [
+    { src: "/GenInfo/adidas.jpg", name: "Adidas", to: "/search/adidas" },
+    { src: "/GenInfo/nike.png", name: "Nike", to: "/search/nike" },
+    { src: "/GenInfo/skechers.jpg", name: "Skechers", to: "/search/skechers" },
+    { src: "/GenInfo/puma.jpg", name: "Puma", to: "/search/puma" },
+  ];
+
+  export default productsData
